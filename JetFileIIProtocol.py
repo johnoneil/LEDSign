06f238cc81bb596d5660fb5df99fca01c9ebb8ae
johnoneil <oneil.john@gmail.com>
2013-03-10 15:14:52 -0700

    Updated JetfileII protocol support. More functionality and better implemented.

--- JetFileIIProtocol.py
+++ JetFileIIProtocol.py
@@ -3,34 +3,79 @@
 #
 # JetFileIIProtocol.py
 # John O'Neil
-# Saturday, March 9th 2012
+# Saturday, March 9th 2013
 #
 # (simplified)JetfileII LED sign protocol derived off documentation
 #
 #******************************************************************************
 
-class Flash:
-  start = '\x071'
-  stop = '\x070'
-
 class Animate:
-  class In:
-    random = '\x0aI\x2f'
-    jump = '\x0aI\x30'
-    move_left = '\x0aI\x31'
-    move_right = '\x0aI\x32'
-    scroll_left = '\x0aI\x33'
-    scroll_right = '\x0aI\x34'
-  class Out:
-    random = '\x0aO\x2f'
-    jump = '\x0aO\x30'
-    move_left = '\x0aO\x31'
-    move_right = '\x0aO\x32'
-    scroll_left = '\x0aO\x33'
-    scroll_right = '\x0aO\x34'
+  class Random:
+    In = '\x0aI\x2f'
+    Out = '\x0aO\x2f'
+  class Jump:#'Jump' is really no "no animation" but None is a reserved keyword, so keeping 'jump'.
+    In = '\x0aI\x30'
+    Out = jump = '\x0aO\x30'
+  class MoveLeft:
+    In = '\x0aI\x31'
+    Out = '\x0aO\x31'
+  class MoveRight:
+    In = '\x0aI\x32'
+    Out = '\x0aO\x32'
+  class WipeLeft:#called "scroll left" in documentation, but is really a wipe
+    In = '\x0aI\x33'
+    Out = '\x0aO\x33'
+  class WipeRight:#called "scroll right" in documentation, but is really a wipe
+    In = '\x0aI\x34'
+    Out = '\x0aO\x34'
+  class Pause:
+    @staticmethod
+    def Seconds(seconds):
+      #TODO: Better handling of seconds values greater than 99
+      return '\x0e0{0:02d}'.format(seconds)
 
-NewFrame = '\x0c'
-NewLine = '\x0d'
+class Format:
+  NewFrame = '\x0c'
+  NewLine = '\x0d'
+  Halfspace = '\x82'
+  class Flash:
+    Start = '\x071'
+    Stop = '\x070'
+  class AutoTypeset:
+    Off = '\x1b0a'
+    On = '\x1b0b'
+  class Background:
+    Black = '\x1d0'
+    Red = '\x1d1'
+    Green = '\x1d2'
+    Amber = '\x1d3'
+  class Align:
+    class Vertical:
+      Center = '\x1f0'
+      Top = '\x1f1'
+      Bottom = '\x1f2'
+    class Horizontal:
+      Center = '\x1e0'
+      Left = '\x1e1'
+      Right = '\x1e2'
+
+class Date:
+  class MMDDYY:
+    WithForwardSlashes = '\x0b\x20'
+    WithDashes = '\x0b\x22'
+    WithDots = '\x0b\x24'
+  class DDMMYY:
+    WithForwardSlashes = '\x0b\x21'
+    WithDashes = '\x0b\x23'
+  YY = '\x0b\x25'
+  YYYY = '\x0b\x26'
+  class Month:
+    Number = '\x0b\x27'
+    Abbreviation = '\x0b\x28'
+  Day = '\x0b\x29'#day of month as two digit number
+  class DayOfWeek:
+    Number = '\x0b\x2a'
+    Abbreviation = '\x0b\x2b'
 
 class Font:
   n5x5 = '\x1a0'
@@ -43,5 +88,25 @@ class Font:
   n12x7 = '\x1a;'
   n22x18 = '\x1a<'
   n30x18 = '\x1a+'
+  n40x21 = '\x1a>'
+  b14x10 = '\x1aN'
+  b15x10 = '\x1aO'
+  b16x12 = '\x1aP'
+  b24x8 = '\x1aQ'
+  b32x8 ='\x1aR'
+  b11x7 = '\x1aS'
+  b12x7 = '\x1aT'
+  b22x12 = '\x1aU'
+  b40x21 = '\x1aV'
+  class Color:
+    Black = '\x1c\x30'
+    Red = '\x1c\x31'
+    Green = '\x1c\x32'
+    Amber = '\x1c\x33'
+    class Mixed:
+      Characters = '\x1c\x34'
+      Horizontal = '\x1c\x35'
+      Wave = '\x1c\x36'
+      Splash = '\x1c\x37'
   
 
--- ledsign.py
+++ ledsign.py
@@ -15,6 +15,8 @@
 import serial
 from JetFileIIProtocol import Font
 from JetFileIIProtocol import Animate
+from JetFileIIProtocol import Format
+from JetFileIIProtocol import Date
 
 
 #currently static values
@@ -26,19 +28,6 @@ widthPixels = 128
 heightPixels = 32
 filename = str('%c%c' % (65 + int(unitAddr / 26), 65 + (unitAddr % 26)))
 
-#Simple character based JetFileII protocol
-#see section 3.1 'Communication format without CheckSum'
-#data = '\x00\x00\x00\x00\x00\x01Z00\x02A\x0f%(drive)cT%(file)s\x06'
-#                '\x0aI%(move_in)c'    '\x0aO%(move_out)c' '\x0e20004'
-#                '\x1b0%(typeset)c'    '\x081'             '\x1f%(vert_align)c'
-#                '\x1e%(horiz_align)c' '\x0f%(speed)c'     '\x1c%(color)c'
-#                '\x1d%(background)c'  '\x1a1'     '\x07%(flash)c'
-#                '%(text)s'            '\x04'
-#data = '\x00\x00\x00\x00\x00\x01Z00\x02A\x0fET%(filename)s\x06Testing my\x0dsoftware now!\x04'% {'filename' : '%c%c' % (65 + int(unitAddr / 26), 65 + (unitAddr % 26))}
-#<0x0a>I1ABCD2345<0x0D>EFGH6789<0x0D>IJKL0123
-#data = '\x00\x00\x00\x00\x00\x01Z00\x02A\x0fET%(filename)s\x06\x0aI1\x0aO1ABCD2345\x0dEFGH6789\x0dIJKL0123\x04'% {'filename' : '%c%c' % (65 + int(unitAddr / 26), 65 + (unitAddr % 26))}
-#data = '\x00\x00\x00\x00\x00\x01Z00\x02A\x0fET%(filename)s\x06\x0aI1\x0aO1BananaTreeGator\x04'% {'filename' : '%c%c' % (65 + int(unitAddr / 26), 65 + (unitAddr % 26))}
-#data = '\x00\x00\x00\x00\x00\x01Z00\x02A\x0fET%(filename)s\x06\x0aI1\x0aO1\x1aN\x1c\x36ABCD2345\x0dEFGH6789\x0dIJKL0123\x04'% {'filename' : '%c%c' % (65 + int(unitAddr / 26), 65 + (unitAddr % 26))}
 HEADER = '\x00\x00\x00\x00\x00\x01Z00'
 PROTOCOL = '\x06'
 CODA = '\x04'
@@ -60,7 +49,7 @@ SPEED_SLOWEST = '\x0f6'
 SPEED_SLOW = '\x0f5'
 SPEED_FAST = '\x0f1' 
 SPEED_FASTEST = '\x0f0' 
-data = HEADER + '\x02A\x0fET' + filename + PROTOCOL + SPEED_FASTEST + MOVE_LEFT_IN + MOVE_LEFT_OUT +  Font.n7x6 + COLOR_RED + '1234abcd'+ NL + COLOR_GREEN + MOVE_RIGHT_IN + MOVE_RIGHT_OUT + SPEED_SLOW + Font.n14x8 + 'EFGH6789' + NL + MOVE_LEFT_IN + MOVE_LEFT_OUT + SPEED_FASTEST + COLOR_AMBER + Font.n5x5 + 'IJKL0123' + CODA
+data = HEADER + '\x02A\x0fET' + filename + PROTOCOL + Format.AutoTypeset.Off + SPEED_FASTEST + Animate.MoveRight.In + Animate.WipeRight.Out +  Font.n7x6 + Font.Color.Red +'1234abcd'+ Format.NewLine + Animate.MoveLeft.In + Animate.MoveLeft.Out + SPEED_SLOW + Font.n14x8 + Font.Color.Green+ Animate.Pause.Seconds(10) + Date.DayOfWeek.Abbreviation + " " + Date.Month.Abbreviation + " " + Date.Day + ", " + Date.YYYY + " " + Format.NewLine + Format.Align.Vertical.Bottom + Format.Align.Horizontal.Left + Animate.Jump.In + Animate.Jump.Out + SPEED_FASTEST + Font.Color.Amber + Font.n5x5 + 'IJKL0123' + CODA
 print str(data)
 
 ser = serial.Serial(port, baudRate)
