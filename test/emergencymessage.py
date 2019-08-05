20176e3117f040378e3232dabb0a21b3da8c5c51
on-three <on.three.email@gmail.com>
2019-08-05 10:21:09 -0700

    Improved emergency message script

--- test/emergencymessage.py
+++ test/emergencymessage.py
@@ -3,37 +3,24 @@
 
 #******************************************************************************
 #
-# weather.py
-# John O'Neil
-# Monday, March 11th, 2013
-#
-# Fetch weather via pywapi (python weather api) draw data out of JSON
-# and send it to my LED sign.
-# This is a test in preparation for an integrated LED sign server type daemon
-# that could have something like this as just one small dedicated 'process'.
-#
-# REQUIRES: debian package "python-pywapi" available in Ubuntu 12.04 or
-# google code.
+#   emergencymessage.py
+#   Hit sign with an async emergency message
 #
 #******************************************************************************
 
 import serial
 import time
-import datetime
-from time import mktime
-from datetime import datetime
 from JetFileII import Message
-displayMsg = Message.DisplayControlWithoutChecksum
 
+msg = Message.EmergencyMessage('This is an Emergency!')
 
-msg = Message.EmergencyMessage('Hello there. How are you?')
-
-print msg.encode("hex")
-
-print 'size of message is ' + str(len(msg))
-
-port = '/dev/ttyUSB0'
+#port = '/dev/ttyUSB0'
+port = '/dev/ttyACM0'
+#port = '/dev/ttyVIRTUAL'
 baudRate = 19200
+
 ser = serial.Serial(port, baudRate)
-x = ser.write(msg)
+
+ser.write(msg)
+
 ser.close()
