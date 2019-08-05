209a88b7810a645dfcd2f5b61a71458abbbc9849
on-three <on.three.email@gmail.com>
2019-08-05 11:45:41 -0700

    updated writetext script but not tested

--- test/writetext.py
+++ test/writetext.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 # vim: set ts=2 expandtab:
 
+'''
+Write a text file using v2.0 protocol with checksum
+v2.0 supports filenames up to 12 characters
+'''
+
 import serial
 from JetFileII import Message
+from JetFileII import TextFile
 
 
-msg = Message.WriteText("Hello there. this is a new writefie implementation.")
+file = TextFile('{pause=5}{middle}{moveRightIn}{moveLeftOut}{b16x12}{green}{hhmin_12hr}{nl}{amber}{7x6}{dow_abbr}, {month_abbr} {date} {yyyy}', "AB.nmg", drive='D')
 
-#print msg.encode("hex")
-
-#print 'size of message is ' + str(len(msg))
 
 #port = '/dev/ttyS0'
 port = '/dev/ttyUSB0'
+#port = '/dev/ttyACM0'
+
 baudRate = 19200
 ser = serial.Serial(port, baudRate)
-x = ser.write(msg)
+x = ser.write(Message.WriteTextFilewithChecksum(file))
 ser.close()
