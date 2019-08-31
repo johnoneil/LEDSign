729b5f44f04a8fcb48dd2ae36526c30017751660
on-three <on.three.email@gmail.com>
2019-08-31 14:37:16 -0700

    Motion sensor code works with GPIO emulator

--- test/motion-sensor-gpio-test.py
+++ test/motion-sensor-gpio-test.py
@@ -7,23 +7,31 @@ from EmulatorGUI import GPIO
 import time
 import traceback
 
+MOTION_SENSOR_GPIO_PIN = 23
+
 def Main():
 
     if True:
         # after: https://www.hackster.io/hardikrathod/pir-motion-sensor-with-raspberry-pi-415c04
         try:
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(23, GPIO.IN) #PIR
-            GPIO.setup(24, GPIO.OUT) #BUzzer
+            GPIO.setup(MOTION_SENSOR_GPIO_PIN, GPIO.IN) #PIR
+            
+            # Optionally drive a buzzer output
+            #GPIO.setup(24, GPIO.OUT) #BUzzer
 
             time.sleep(2) # to stabilize sensor
             while True:
-                if GPIO.input(23):
-                    GPIO.output(24, True)
-                    time.sleep(0.5) #Buzzer turns on for 0.5 sec
-                    GPIO.output(24, False)
+                if GPIO.input(MOTION_SENSOR_GPIO_PIN): # PIR motion sensor goes high
+                    
+                    # Optional buzzer output
+                    #GPIO.output(24, True)
+                    #time.sleep(0.5) #Buzzer turns on for 0.5 sec
+                    #GPIO.output(24, False)
+
                     print("Motion Detected...")
                     time.sleep(5) #to avoid multiple detection
+                
                 time.sleep(0.1) #loop delay, should be less than detection delay
         except Exception as ex:
             traceback.print_exc()
