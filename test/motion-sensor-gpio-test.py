889ada9d9a0a9b1ea42831e1abc39b6251369007
on-three <on.three.email@gmail.com>
2019-09-01 11:01:24 -0700

    Working GPI scan with dbus integration

--- test/motion-sensor-gpio-test.py
+++ test/motion-sensor-gpio-test.py
@@ -9,70 +9,95 @@ else:
 import time
 import traceback
 
+import dbus, dbus.service, dbus.exceptions
+import sys
+
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+from gi.repository import GObject as gobject
+
+DBUS_INTERFACE_NAME = 'com.MotionDetectService'
+DBUS_OBJECT_PATH = '/com/motiondetect/object'
+
 MOTION_SENSOR_GPIO_PIN = 23
 
-def Main():
+init_gpio = True
+obj = None
 
-    if True:
-        # after: https://www.hackster.io/hardikrathod/pir-motion-sensor-with-raspberry-pi-415c04
-        try:
+def emit_signal():
+    bus = dbus.SessionBus()
+    try:
+        obj  = bus.get_object(DBUS_INTERFACE_NAME, DBUS_OBJECT_PATH)
+
+        obj.emitMotionDetectSignal(dbus_interface=DBUS_INTERFACE_NAME)
+    except dbus.DBusException:
+        traceback.print_exc()
+        sys.exit(1)
+    
+    #reply_handler = handle_reply, error_handler = handle_error)
+    return False
+
+def catchall_motiondetect_signals_handler(hello_string):
+    print ("Received a hello signal and it says " + hello_string)
+
+def checkGPIO():
+    global init_gpio
+    global obj
+    # after: https://www.hackster.io/hardikrathod/pir-motion-sensor-with-raspberry-pi-415c04
+    try:
+        if init_gpio:
+            init_gpio = False
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(MOTION_SENSOR_GPIO_PIN, GPIO.IN) #PIR
-            
+        
             # Optionally drive a buzzer output
             #GPIO.setup(24, GPIO.OUT) #BUzzer
 
             time.sleep(2) # to stabilize sensor
-            while True:
-                if GPIO.input(MOTION_SENSOR_GPIO_PIN): # PIR motion sensor goes high
-                    
-                    # Optional buzzer output
-                    #GPIO.output(24, True)
-                    #time.sleep(0.5) #Buzzer turns on for 0.5 sec
-                    #GPIO.output(24, False)
+        if GPIO.input(MOTION_SENSOR_GPIO_PIN): # PIR motion sensor goes high
 
-                    print("Motion Detected...")
-                    time.sleep(5) #to avoid multiple detection
-                
-                time.sleep(0.1) #loop delay, should be less than detection delay
-        except Exception as ex:
-            traceback.print_exc()
-        finally:
-            GPIO.cleanup() #this ensures a clean exit
-    else:
-        try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setwarnings(False)
-            GPIO.setup(4, GPIO.OUT)
-            GPIO.setup(17, GPIO.OUT, initial = GPIO.LOW)
-            GPIO.setup(18, GPIO.OUT, initial = GPIO.LOW)
-            GPIO.setup(21, GPIO.OUT, initial = GPIO.LOW)
-            GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-            GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-            GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-            GPIO.setup(26, GPIO.IN)
+            print("Motion Detected...")
+            emit_signal()
 
-            while True:
-                if (GPIO.input(23) == False):
-                    GPIO.output(4,GPIO.HIGH)
-                    GPIO.output(17,GPIO.HIGH)
-                    time.sleep(1)
-                if (GPIO.input(15) == True):
-                    GPIO.output(18,GPIO.HIGH)
-                    GPIO.output(21,GPIO.HIGH)
-                    time.sleep(1)
-                if (GPIO.input(24) == True):
-                    GPIO.output(18,GPIO.LOW)
-                    GPIO.output(21,GPIO.LOW)
-                    time.sleep(1)
-                if (GPIO.input(26) == True):
-                    GPIO.output(4,GPIO.LOW)
-                    GPIO.output(17,GPIO.LOW)
-                    time.sleep(1)
+            # Optional buzzer output
+            #GPIO.output(24, True)
+            #time.sleep(0.5) #Buzzer turns on for 0.5 sec
+            #GPIO.output(24, False)
 
-        except Exception as ex:
-            traceback.print_exc()
-        finally:
-            GPIO.cleanup() #this ensures a clean exit
+            #time.sleep(5) #to avoid multiple detection
+        
+        #time.sleep(0.1) #loop delay, should be less than detection delay
+    except Exception as ex:
+        traceback.print_exc()
+        return False
+    return True
 
-Main()
+def Main():
+    init_gpio = True
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    bus = dbus.SessionBus()
+    try:
+        obj  = bus.get_object(DBUS_INTERFACE_NAME, DBUS_OBJECT_PATH)
+
+        obj.connect_to_signal("MotionDetectSignal", catchall_motiondetect_signals_handler, dbus_interface=DBUS_INTERFACE_NAME, arg0="Hello")
+    except dbus.DBusException:
+        traceback.print_exc()
+        sys.exit(1)
+
+    #lets make a catchall
+    #bus.add_signal_receiver(catchall_signal_handler, interface_keyword='dbus_interface', member_keyword='member')
+
+    bus.add_signal_receiver(catchall_motiondetect_signals_handler, dbus_interface = DBUS_INTERFACE_NAME, signal_name = "MotionDetectSignal")
+
+    #bus.add_signal_receiver(catchall_motiondetectservice_interface_handler, dbus_interface = DBUS_INTERFACE_NAME, message_keyword='dbus_message')
+
+    gobject.idle_add(checkGPIO, priority=gobject.PRIORITY_HIGH)
+
+    # Initialize a main loop
+    DBusGMainLoop(set_as_default=True)
+    loop = GLib.MainLoop()
+    loop.run()
+
+if __name__ == '__main__':
+    Main()
